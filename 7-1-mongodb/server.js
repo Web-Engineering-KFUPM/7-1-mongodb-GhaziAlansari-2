1b002a2a36ffffb98f730c4adce32696f40a4b8a
= <=>
2026-04-13 21:33:12 +0300

    week7.1

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -185,20 +185,57 @@
  */
 
 // import mongoose
-
+import mongoose from "mongoose";
 // establish connection
+
+mongoose.connect("mongodb+srv://Alansarigmk33:REUN1mZ9pGbBnj5s@cluster0.rkjwnyf.mongodb.net/labDB")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
 
 // define schema
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String
+});
 
+const Student = mongoose.model("Student", studentSchema);
 
 // create document
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" }
+  ]);
+  console.log("Inserted students");
+}
 
+createStudents();
 
 // read document
+async function readStudents() {
+  const students = await Student.find();
+  console.log(students);
+}
 
+readStudents();
 
 // update document
+async function updateStudent() {
+  await Student.updateOne(
+    { name: "Ali" },
+    { age: 22 }
+  );
+  console.log("Updated Ali");
+}
 
+updateStudent();
 
 // delete document
+async function deleteStudent() {
+  await Student.deleteOne({ name: "Sara" });
+  console.log("Deleted Sara");
+}
+
+deleteStudent();
